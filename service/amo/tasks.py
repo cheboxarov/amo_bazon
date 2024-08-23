22c085ce0b4c9ccf139ca0d0097dac48e41521d4
Acetonerer <bolokan2014@gmail.com>
2024-08-23 10:33:50 +0700

    update shared_task

--- service/amo/tasks.py
+++ service/amo/tasks.py
@@ -1,51 +1,57 @@
 from celery import shared_task
 from .models import AmoAccount, Status, Manager
+from amocrm.v2 import tokens, pipelines, users
 
 
 @shared_task
 def sync_amo_data():
     for amo_account in AmoAccount.objects.all():
-        client = AmoCRMClient(token=amo_account.token, subdomain=amo_account.suburl)
+        # Подключение к amoCRM для каждого аккаунта
+        tokens.default_token_manager(
+            access_token=amo_account.token,  # Токен из базы данных
+            subdomain=amo_account.suburl,  # URL из базы данных
+            storage=tokens.FileTokensStorage()  # Выберите нужный способ хранения токенов
+        )
 
-        # Синхронизация статусов
-        statuses_data = client.get_statuses()
+        # Синхронизация статусов (воронки)
+        pipelines_data = list(pipelines.all())  # Получение всех воронок
         existing_status_ids = Status.objects.filter(amo_account=amo_account).values_list('amo_id', flat=True)
 
-        for pipeline in statuses_data['_embedded']['pipelines']:
-            for status_data in pipeline['statuses']:
-                if status_data['id'] in existing_status_ids:
+        for pipeline in pipelines_data:
+            for status_data in pipeline.statuses:
+                if status_data.id in existing_status_ids:
                     # Обновление существующего статуса
-                    status = Status.objects.get(amo_id=status_data['id'], amo_account=amo_account)
-                    status.name = status_data['name']
+                    status = Status.objects.get(amo_id=status_data.id, amo_account=amo_account)
+                    status.name = status_data.name
                     status.save()
                 else:
                     # Добавление нового статуса
                     Status.objects.create(
-                        amo_id=status_data['id'],
-                        name=status_data['name'],
+                        amo_id=status_data.id,
+                        name=status_data.name,
                         amo_account=amo_account
                     )
 
         # Удаление статусов, которых больше нет в amoCRM
-        Status.objects.filter(amo_account=amo_account).exclude(amo_id__in=[s['id'] for p in statuses_data['_embedded']['pipelines'] for s in p['statuses']]).delete()
+        Status.objects.filter(amo_account=amo_account).exclude(amo_id__in=[s.id for p in pipelines_data for s in p.statuses]).delete()
 
         # Синхронизация менеджеров
-        managers_data = client.get_managers()
+        managers_data = list(users.all())  # Получение всех менеджеров
         existing_manager_ids = Manager.objects.filter(amo_account=amo_account).values_list('amo_id', flat=True)
 
-        for manager_data in managers_data['_embedded']['users']:
-            if manager_data['id'] in existing_manager_ids:
+        for manager_data in managers_data:
+            if manager_data.id in existing_manager_ids:
                 # Обновление существующего менеджера
-                manager = Manager.objects.get(amo_id=manager_data['id'], amo_account=amo_account)
-                manager.name = manager_data['name']
+                manager = Manager.objects.get(amo_id=manager_data.id, amo_account=amo_account)
+                manager.name = manager_data.name
                 manager.save()
             else:
                 # Добавление нового менеджера
                 Manager.objects.create(
-                    amo_id=manager_data['id'],
-                    name=manager_data['name'],
+                    amo_id=manager_data.id,
+                    name=manager_data.name,
                     amo_account=amo_account
                 )
 
         # Удаление менеджеров, которых больше нет в amoCRM
-        Manager.objects.filter(amo_account=amo_account).exclude(amo_id__in=[m['id'] for m in managers_data['_embedded']['users']]).delete()
+        Manager.objects.filter(amo_account=amo_account).exclude(amo_id__in=[m.id for m in managers_data]).delete()
