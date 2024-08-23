29556927fc9f641522488c5b928e3ac0e7e792b2
Acetonerer <bolokan2014@gmail.com>
2024-08-23 17:09:48 +0700

    fix amo models.py

--- service/bazon/events/contractors.py
+++ service/bazon/events/contractors.py
@@ -4,5 +4,6 @@ from bazon.models import Contractor
 def on_create_contractor(contractor: Contractor):
     print(f"Contractor created: {contractor.name}")
 
+
 def on_update_contractor(contractor: Contractor):
     print(f"Contractor updated: {contractor.name}")
--- service/bazon/events/sale_documents.py
+++ service/bazon/events/sale_documents.py
@@ -4,5 +4,6 @@ from bazon.models import SaleDocument
 def on_create_sale_document(sale: SaleDocument):
     print(f"новый документ {sale.internal_id}")
 
+
 def on_update_sale_document(sale: SaleDocument):
     print(f"sale document updated {sale.internal_id}")
