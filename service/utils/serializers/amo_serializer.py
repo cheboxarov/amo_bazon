44f2eef71f8ca795fa0ce80da2e6551b0dd11d26
Acetonerer <bolokan2014@gmail.com>
2024-08-26 14:42:52 +0700

    fix status and managers in amo_serializer.py

--- service/utils/serializers/amo_serializer.py
+++ service/utils/serializers/amo_serializer.py
@@ -1,3 +1,4 @@
+from amo.models import Status, Manager
 from .base_serializer import BaseSerializer
 
 
@@ -10,8 +11,17 @@ class AmoContactToBazonContactSerializer(BaseSerializer):
         serialized_data = {}
         serialized_data["contact_id"] = self.data.get('id')
         serialized_data["company_id"] = self.data.get("linked_company_id")
-        serialized_data["responsible_user_id"] = self.data.get("manager_id")
 
+        status_id = self.data.get("status_id")
+        if Status.objects.filter(amo_id=status_id).exists():
+            status = Status.objects.get(amo_id=status_id)
+            serialized_data["status"] = status.bazon_status
+
+        self._serialized_data = serialized_data
+        responsible_user_id = self.data.get("responsible_user_id")
+        if Manager.objects.filter(amo_id=responsible_user_id).exists():
+            manager = Manager.objects.get(amo_id=responsible_user_id)
+            serialized_data["manager_id"] = manager.bazon_id
         self._serialized_data = serialized_data
 
     def get_serialized_data(self, with_id: bool = True):
