650c56f407d70cb674f05eb7159161a3098fdac7
cheboxarov <lalakasuper2@gmail.com>
2024-09-02 11:31:34 +0700

    Сделал хендлеры для вебхуков с амо

--- service/amo/amo_webhooks/on_lead_status.py
+++ service/amo/amo_webhooks/on_lead_status.py
@@ -5,17 +5,21 @@ from django.db.models import ObjectDoesNotExist
 def on_lead_status_handler(data):
     lead_id_arr = data.get("leads[status][0][id]", [])
     if len(lead_id_arr) == 0:
+        print(lead_id_arr)
         return
     lead_id = lead_id_arr[0]
     lead_status_id_arr = data.get("leads[status][0][status_id]", [])
     if len(lead_status_id_arr) == 0:
+        print(lead_status_id_arr)
         return
     lead_status_id = lead_status_id_arr[0]
     try:
         amo_status = Status.objects.get(amo_id=int(lead_status_id))
     except ObjectDoesNotExist:
+        print("Объект не найден")
         return
     bazon_status = amo_status.bazon_status
     if bazon_status is None:
+        print(bazon_status)
         return
     print(bazon_status)
