5be282b77992bb1f8b2a91af4d15a9ecb8a6ad6f
cheboxarov <lalakasuper2@gmail.com>
2024-09-03 11:11:52 +0700

    Кеширование лок кея

--- service/bazon/views.py
+++ service/bazon/views.py
@@ -129,7 +129,7 @@ class BazonItemsAddView(APIView):
             lock_key = response.json().get("response", {}).get("setDocumentLock", {}).get("lockKey")
             if not isinstance(lock_key, str):
                 response = bazon_api.drop_lock_key(sale_document.internal_id, "")
-                print(response)
+                print(response.json())
                 return Response({"Error": "Cant get lock key"}, status=HTTP_502_BAD_GATEWAY)
             cache.set(sale_document.number, lock_key, timeout=30)
         print(lock_key)
