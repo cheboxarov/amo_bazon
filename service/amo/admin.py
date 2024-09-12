cceba6e7c7c8807f8f3a86493d5613fdd85e773c
cheboxarov <lalakasuper2@gmail.com>
2024-09-12 15:01:10 +0700

    Изменил админку в amo app

--- service/amo/admin.py
+++ service/amo/admin.py
@@ -2,7 +2,12 @@ from django.contrib import admin
 from .models import AmoAccount, Status, Manager
 
 
-# Register your models here.
 admin.site.register(AmoAccount)
-admin.site.register(Status)
-admin.site.register(Manager)
+
+@admin.register(Status)
+class StatusAdmin(admin.ModelAdmin):
+    fields = ("name", "amo_account", "bazon_status")
+
+@admin.register(Manager)
+class AdminManager(admin.ModelAdmin):
+    fields = ("name", "amo_account")
--- service/amo/models.py
+++ service/amo/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 
 class AmoAccount(models.Model):
-    suburl = models.CharField(max_length=255, unique=True)
-    token = models.TextField()
-    bazon_accounts = models.ManyToManyField('bazon.BazonAccount', related_name='amo_accounts')
+    suburl = models.CharField(max_length=255, unique=True, verbose_name="Поддомен")
+    token = models.TextField(verbose_name="Токен")
+    bazon_accounts = models.ManyToManyField('bazon.BazonAccount', related_name='amo_accounts', verbose_name="Аккаунты Bazon")
 
     def __str__(self):
         return self.suburl
@@ -21,10 +21,10 @@ class Status(models.Model):
         ('canceled', 'Не реализованные'),
     ]
 
-    amo_id = models.IntegerField()
-    name = models.CharField(max_length=255)
-    bazon_status = models.CharField(max_length=20, choices=BAZON_STATUSES, blank=True, null=True)
-    amo_account = models.ForeignKey(AmoAccount, on_delete=models.CASCADE, related_name='statuses')
+    amo_id = models.IntegerField(verbose_name="ID в AmoCRM")
+    name = models.CharField(max_length=255, verbose_name="Имя")
+    bazon_status = models.CharField(max_length=20, choices=BAZON_STATUSES, blank=True, null=True, verbose_name="Статус в Bazon")
+    amo_account = models.ForeignKey(AmoAccount, on_delete=models.CASCADE, related_name='statuses', verbose_name="Аккаунт в AmoCRM")
 
     def __str__(self):
         return self.name
@@ -35,10 +35,10 @@ class Status(models.Model):
 
 
 class Manager(models.Model):
-    name = models.CharField(max_length=255)
-    amo_id = models.IntegerField()
-    bazon_id = models.IntegerField(blank=True, null=True)
-    amo_account = models.ForeignKey(AmoAccount, on_delete=models.CASCADE, related_name='managers')
+    name = models.CharField(max_length=255, verbose_name="Имя")
+    amo_id = models.IntegerField(verbose_name="ID в AmoCRM")
+    bazon_id = models.IntegerField(blank=True, null=True, verbose_name="ID в Bazon")
+    amo_account = models.ForeignKey(AmoAccount, on_delete=models.CASCADE, related_name='managers', verbose_name="Аккаунт AmoCRM")
 
     def __str__(self):
         return self.name
