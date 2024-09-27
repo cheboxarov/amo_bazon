3d2f92f92c27cdf242b67370b13db871d0289f5d
wlove <lalakasuper2@gmail.com>
2024-09-27 12:46:20 +0700

    При пуллинге теперь таски идут как транзакции, создание документа внутри транзакции идет

---
+++ service/utils/transaction.py
@@ -0,0 +1,8 @@
+from django.db import transaction
+
+
+def transaction_decorator(func):
+    def wrapper(*args, **kwargs):
+        with transaction.atomic():
+            return func(*args, **kwargs)
+    return wrapper
