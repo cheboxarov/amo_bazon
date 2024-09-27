0ba0bdf55a38d77e6eff62301f2a1e2e79b782f3
wlove <lalakasuper2@gmail.com>
2024-09-27 15:06:47 +0700

    Сделал синхронный воркер обработки сделок

--- service/bazon/events/contractors.py
+++ service/bazon/events/contractors.py
@@ -21,7 +21,7 @@ class _Contractor(BaseModel):
 
 
 def on_create_contractor(contractor_data: dict, amo_account: AmoAccount, bazon_account: BazonAccount):
-    if Contractor.objects.filter(internal_id=contractor_data.get("id")).exists():
+    if Contractor.objects.filter(internal_id=contractor_data.get("id"), amo_account=amo_account).exists():
         return
     print(_Contractor.model_validate(contractor_data).model_dump())
     contractor = Contractor.objects.create(amo_account=amo_account,
