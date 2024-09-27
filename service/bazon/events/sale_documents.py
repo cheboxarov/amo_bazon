514524fb59c96ddc025b3be3f6eef791a138f7e5
wlove <lalakasuper2@gmail.com>
2024-09-27 16:14:35 +0700

    Сделал синхронный воркер обработки сделок

--- service/bazon/events/sale_documents.py
+++ service/bazon/events/sale_documents.py
@@ -4,38 +4,40 @@ from amo.models import AmoAccount
 from amo.amo_client import DealClient
 from bazon.models import SaleDocument, Contractor
 from .contractors import on_create_contractor
+from django.db import transaction
 
 
 def on_create_sale_document(
     sale_data: dict,
-    amo_account: AmoAccount,
-    sale_document: SaleDocument
+    amo_account: AmoAccount
 ):
-    serializer = BazonSaleToAmoLeadSerializer(amo_account, sale_data)
-    serializer.serialize()
-    serialized_data = serializer.get_serialized_data(with_id=False)
-    amo_client = DealClient(amo_account.token, amo_account.suburl)
-    response = amo_client.create_deal(**serialized_data)
-    amo_lead_id = response["_embedded"]["leads"][0]["id"]
-    sale_document.amo_lead_id = amo_lead_id
-    sale_document.amo_account = amo_account
-    sale_document.save()
+    with transaction.atomic():
+        sale_document = SaleDocument.objects.filter(internal_id=sale_data.get("internal_id"), amo_account=amo_account).first()
+        serializer = BazonSaleToAmoLeadSerializer(amo_account, sale_data)
+        serializer.serialize()
+        serialized_data = serializer.get_serialized_data(with_id=False)
+        amo_client = DealClient(amo_account.token, amo_account.suburl)
+        response = amo_client.create_deal(**serialized_data)
+        amo_lead_id = response["_embedded"]["leads"][0]["id"]
+        sale_document.amo_lead_id = amo_lead_id
+        sale_document.amo_account = amo_account
+        sale_document.save()
 
-    if sale_document.contractor_id:
-        api = sale_document.get_api()
-        contractor_response = api.get_contractor(sale_document.contractor_id)
-        contractor_json = (contractor_response.json()
-                           .get("response", {})
-                           .get("getContractor", {})
-                           .get("Contractor"))
-        if contractor_json is None:
-            return
-        query = Contractor.objects.filter(internal_id=sale_document.contractor_id, amo_account=amo_account)
-        if not query.exists():
-            on_create_contractor(contractor_json,
-                                 bazon_account=sale_document.bazon_account,
-                                 amo_account=sale_document.amo_account)
-        contractor = query.first()
+        if sale_document.contractor_id:
+            api = sale_document.get_api()
+            contractor_response = api.get_contractor(sale_document.contractor_id)
+            contractor_json = (contractor_response.json()
+                            .get("response", {})
+                            .get("getContractor", {})
+                            .get("Contractor"))
+            if contractor_json is None:
+                return
+            query = Contractor.objects.filter(internal_id=sale_document.contractor_id, amo_account=amo_account)
+            if not query.exists():
+                on_create_contractor(contractor_json,
+                                    bazon_account=sale_document.bazon_account,
+                                    amo_account=sale_document.amo_account)
+            contractor = query.first()
 
 def on_update_sale_document(sale_data: dict, amo_account: AmoAccount):
     print(f"Deal updated: {sale_data}")
--- service/bazon/management/commands/sale_documents_polling.py
+++ service/bazon/management/commands/sale_documents_polling.py
@@ -6,7 +6,6 @@ from bazon.events import (
     on_create_sale_document,
     on_update_sale_document,
 )
-from django.db import transaction
 from django.core.management.base import BaseCommand
 
 
@@ -29,37 +28,37 @@ class Command(BaseCommand):
                     return
                 
                 for amo_account in bazon_account.amo_accounts.all():
-                    with transaction.atomic():
-                        data = response.json()
-                        for json_document in data["response"][0]["result"]["sale_documents"]:
-                            try:
-                                json_document["internal_id"] = json_document.pop("id")
-                            except KeyError:
-                                print(json_document)
-                                continue
-                            
-                            json_document["bazon_account"] = bazon_account
-                            
-                            sale_document, created = SaleDocument.objects.get_or_create(
-                                internal_id=json_document["internal_id"],
-                                amo_account=amo_account,
-                                defaults=json_document 
-                            )
-                            
-                            if not created:
-                                document_dict = model_to_dict(sale_document)
-                                document_dict.pop("id")
-                                document_dict.pop("bazon_account")
-                                document_dict.pop("amo_lead_id")
-                                document_dict.pop("amo_account")
-                                document_dict.pop("contractor_linked")
-                                json_document.pop("bazon_account")
+                    data = response.json()
+                    for json_document in data["response"][0]["result"]["sale_documents"]:
+                        try:
+                            json_document["internal_id"] = json_document.pop("id")
+                        except KeyError:
+                            print(json_document)
+                            continue
+                        
+                        json_document["bazon_account"] = bazon_account
+                        
+                        sale_document_query = SaleDocument.objects.filter(
+                            internal_id=json_document["internal_id"],
+                            amo_account=amo_account
+                        )
+                        exists = sale_document_query.exists()
+                        
+                        if exists:
+                            sale_document = sale_document_query.first()
+                            document_dict = model_to_dict(sale_document)
+                            document_dict.pop("id")
+                            document_dict.pop("bazon_account")
+                            document_dict.pop("amo_lead_id")
+                            document_dict.pop("amo_account")
+                            document_dict.pop("contractor_linked")
+                            json_document.pop("bazon_account")
 
-                                if document_dict != json_document:
-                                    for key, value in json_document.items():
-                                        if document_dict.get(key) != value:
-                                            setattr(sale_document, key, value)
-                                    sale_document.save()
-                                    on_update_sale_document(json_document, amo_account)
-                            else:
-                                on_create_sale_document(json_document, amo_account, sale_document)
+                            if document_dict != json_document:
+                                for key, value in json_document.items():
+                                    if document_dict.get(key) != value:
+                                        setattr(sale_document, key, value)
+                                sale_document.save()
+                                on_update_sale_document(json_document, amo_account)
+                        else:
+                            on_create_sale_document(json_document, amo_account, sale_document)
