96a9a4055a848aa60c48c024a1de3ab0914577c4
wlove <lalakasuper2@gmail.com>
2024-10-13 23:30:47 +0700

    Сделал обновление контактов

--- service/bazon/events/contractors.py
+++ service/bazon/events/contractors.py
@@ -21,7 +21,9 @@ class _Contractor(BaseModel):
 
 
 def on_create_contractor(contractor_data: dict, amo_account: AmoAccount, bazon_account: BazonAccount):
+
     logger.debug(f"contractor_data: {_Contractor.model_validate(contractor_data).model_dump()}")
+
     contractor = Contractor.objects.create(amo_account=amo_account,
                                            **_Contractor.model_validate(contractor_data).model_dump(),
                                            bazon_account=bazon_account)
@@ -36,24 +38,72 @@ def on_create_contractor(contractor_data: dict, amo_account: AmoAccount, bazon_a
                 }
             ]
         })
+
     amo_config = json.loads(amo_account.config)
     if (contact_phone_field := amo_config.get("contact_phone_field")) and contractor.phone != "":
         append_value(contact_phone_field, contractor.phone)
     if (contact_email_id := amo_config.get("contact_email_field")) and contractor.email != "":
         append_value(contact_email_id, contractor.email)
+
     logger.debug(f"Перед созданием контакта, данные: {contractor.name} , {custom_fields}")
+
     try:
         amo_contact = (api.create_contact(contractor.name, custom_fields=custom_fields)
                        .get("_embedded",{})).get("contacts", [None])[0]
+        
         logger.debug(f"Создан контакт {amo_contact}")
+
     except Exception as error:
+
         logger.error(f"Error create contact: {error} {custom_fields}")
         return
+    
     if amo_contact is None:
         return
     contractor.amo_id = amo_contact.get("id")
     contractor.save()
 
 
-def on_update_contractor(contractor_data: dict, amo_account: AmoAccount):
-    print(f"Contractor updated: {contractor_data['name']}")
+def on_update_contractor(contractor_data: dict, amo_account: AmoAccount, bazon_account: BazonAccount):
+    logger.debug(f"UPDATE contractor_data: {_Contractor.model_validate(contractor_data).model_dump()}")
+    validated_contractor_data = _Contractor.model_validate(contractor_data).model_dump()
+    contractor = Contractor.objects.get(id=validated_contractor_data.get("internal_id"))
+    contractor.delete()
+    contractor = Contractor.objects.create(amo_account=amo_account,
+                                           **validated_contractor_data,
+                                           bazon_account=bazon_account)
+    api = ContactClient(amo_account.token, amo_account.suburl)
+    custom_fields = []
+    def append_value(field_id: int, value: str):
+        custom_fields.append({
+            "field_id": field_id,
+            "values": [
+                {
+                    "value": value
+                }
+            ]
+        })
+
+    amo_config = json.loads(amo_account.config)
+    if (contact_phone_field := amo_config.get("contact_phone_field")) and contractor.phone != "":
+        append_value(contact_phone_field, contractor.phone)
+    if (contact_email_id := amo_config.get("contact_email_field")) and contractor.email != "":
+        append_value(contact_email_id, contractor.email)
+
+    logger.debug(f"Перед созданием контакта, данные: {contractor.name} , {custom_fields}")
+
+    try:
+        amo_contact = (api.update_contact(contractor.name, custom_fields=custom_fields)
+                       .get("_embedded",{})).get("contacts", [None])[0]
+        
+        logger.debug(f"Обновлен контакт {amo_contact}")
+        
+    except Exception as error:
+
+        logger.error(f"Ошибка в обновлении контакта: {error} {custom_fields}")
+        return
+    
+    if amo_contact is None:
+        return
+    contractor.amo_id = amo_contact.get("id")
+    contractor.save()
--- service/bazon/events/sale_documents.py
+++ service/bazon/events/sale_documents.py
@@ -65,11 +65,46 @@ def on_create_sale_document(
         logger.debug(f"Контакт {contractor.amo_id} прилинкован к сделке {sale_document.amo_lead_id}")
 
 def on_update_sale_document(sale_data: dict, amo_account: AmoAccount):
-    print(f"Deal updated: {sale_data}")
+
+    logger.debug(f"Начало обновления сделки {sale_data}")
+
     serializer = BazonSaleToAmoLeadSerializer(amo_account, sale_data)
     serializer.serialize()
     serialized_data = serializer.get_serialized_data(with_id=True)
     amo_client = DealClient(amo_account.token, amo_account.suburl)
+    
     if serialized_data.get("id") is None:
         return
     response = amo_client.update_deal(**serialized_data)
+
+    sale_document = SaleDocument.objects.get(id=(serialized_data.get("id")))
+    if sale_document.contractor_id:
+        api = sale_document.get_api()
+        contractor_response = api.get_contractor(sale_document.contractor_id)
+        contractor_json = (contractor_response.json()
+                        .get("response", {})
+                        .get("getContractor", {})
+                        .get("Contractor"))
+        if contractor_json is None:
+            return
+        query = Contractor.objects.filter(internal_id=sale_document.contractor_id, amo_account=amo_account)
+        if not query.exists():
+            on_create_contractor(contractor_json,
+                                bazon_account=sale_document.bazon_account,
+                                amo_account=sale_document.amo_account)
+        contractor: Contractor = query.first()
+        if not contractor.amo_id:
+            return
+        
+        amo_client: AmoCRMClient = amo_account.get_amo_client()
+
+        response = amo_client.link_entity(to_type="contacts",
+                                          to_id=contractor.amo_id,
+                                          e_id=sale_document.amo_lead_id,
+                                          e_type="leads")
+        if response.status_code != 200:
+            logger.error(f"(обновление сделки) Ошибка в линковке контакта к сделке, ответ от амо: {response.text}")
+            return
+        logger.debug(f"(Обновление сделки) Контакт {contractor.amo_id} прилинкован к сделке {sale_document.amo_lead_id}")
+
+    
