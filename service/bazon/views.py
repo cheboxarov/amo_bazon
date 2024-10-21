495b7f3b9c7c8a6908cd26d41f07c28b108a6021
wlove <lalakasuper2@gmail.com>
2024-10-21 16:41:19 +0700

    Сделал адекватную актуализацию сделки

--- service/bazon/views.py
+++ service/bazon/views.py
@@ -736,7 +736,8 @@ class BazonSaleUpdate(CustomAPIView, BazonApiMixin, SaleDocumentMixin):
         logger.info(f"[{subdomain}] Начало обработки запроса на обновление сделки")
         sale_document = self.get_sale_document(amo_lead_id)
         bazon_api = sale_document.get_api()
-        sale_document_data = bazon_api.get_detail_document(sale_document.internal_id).json()
+        sale_document_data = bazon_api.get_detail_document(int(sale_document.internal_id)).json()
+        logger.debug(f"Акутализирую сделку - {sale_document_data}")
         document_json = sale_document_data["response"]["getDocument"]["Document"]
         document_json["internal_id"] = document_json.pop("id")
         sale_document_data["internal_id"] = sale_document_data.pop("id")
